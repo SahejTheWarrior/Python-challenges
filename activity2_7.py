da1a68cb10aa1dc3f9c6e756e40b9f110bd6e5b8
SahejTheWarrior <sahejburande@gmail.com>
2025-09-10 19:07:48 +0100

    Add files via upload

---
+++ activity2_7.py
@@ -0,0 +1,9 @@
+#divisable number
+print("enter a number(numerator): ")
+num1=int(input())
+print("enter a number(denominator): ")
+num2=int(input())
+if num1 % num2 ==0:
+    print("\n" +str(num1)+"is it divisable by"+str(num2))
+else:
+    print("\n" +str(num1)+"is it divisable by"+str(num2))
