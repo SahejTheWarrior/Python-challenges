4761853f997f16596f372771ffeecd1337138252
SahejTheWarrior <sahejburande@gmail.com>
2025-09-10 19:18:50 +0100

    Add files via upload

---
+++ activity3_7.py
@@ -0,0 +1,11 @@
+#mean value
+mean1=38
+wrong_number=36
+correct_number=56
+total_num=40
+sum=mean1*total_num
+print("the sum of 40 number : ", sum)
+num2=sum-((wrong_number)-(correct_number))
+print("sum-((wrong_number)-(correct_number))", num2)
+mean2=num2/total_num
+print(mean2)
